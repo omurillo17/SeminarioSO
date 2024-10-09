2d17cbecceb47cbab76047dbb0ea9b052ad8d760
Geronimo Murillo <132098709+omurillo17@users.noreply.github.com>
2024-10-09 17:48:19 -0600

    Update helloworld.bat

--- HandsOn2/helloworld.bat
+++ HandsOn2/helloworld.bat
@@ -1,12 +1,9 @@
 @echo off
 echo Hello World
 echo.
-echo Listando el contenido del directorio actual:
 dir
 echo.
-echo Creando un subdirectorio llamado Test...
 mkdir Test
 cd Test
 echo.
-echo Listando el contenido del subdirectorio Test:
 dir
